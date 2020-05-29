16fece1c076f59b5eac22c34a0c61600f6ac4975
Preyas-Saxena <preyas.saxena@gmail.com>
2020-05-29 16:30:42 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,3 +2,24 @@ import numpy as np
 from keras import optimizers
 import skimage.io as io
 
+from Custom_Model import unet as seg # Importing the Unet model defined earlier
+
+adam = optimizers.Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+seg.compile(optimizer=adam, loss='binary_crossentropy', metrics=['binary_crossentropy'])
+seg.fit(train_x, train_y, epochs=3, batch_size=16, verbose=1, validation_split=0.10)
+
+#6th Step- Visualizing the model results on selected test images
+
+img=train_x[50]
+io.imshow(img)
+
+test=train_y[50]
+test=test.squeeze()
+io.imshow(test, cmap="gray")
+
+img = np.expand_dims(img, 0)
+pred = seg.predict(img, batch_size=1)
+pred=pred.squeeze()
+pred=pred*255
+io.imshow(pred, cmap="gray")
+
