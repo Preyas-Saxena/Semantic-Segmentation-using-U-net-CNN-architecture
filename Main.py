3f28978dcd85c6059aa2c78c4190d17b70d246bf
Preyas-Saxena <preyas.saxena@gmail.com>
2020-05-29 16:44:00 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,12 +2,14 @@
 import numpy as np
 from keras import optimizers
 import skimage.io as io
+import Data_Augmentation
+import Data_processing
 from Custom_Model import unet as seg # Importing the Unet model defined earlier
 from Loss_functions import dice_coef, dice_coef_loss
 
 #Generating the dataset from trainval files, and augmenting it
-total_x, total_y= build_seg_onemask_dataset('trainval.txt')
-aug_x, aug_y= augment(total_x, total_y)
+total_x, total_y= Data_processing.build_seg_onemask_dataset('trainval.txt')
+aug_x, aug_y= Data_Augmentation.augment(total_x, total_y)
 
 #Training the model with binary cross entropy (or dice loss) for 5 epochs:
 adam = optimizers.Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
