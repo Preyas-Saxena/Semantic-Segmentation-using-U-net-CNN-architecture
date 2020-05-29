e07d465b1335ca9d57523b83ff34d6cb2ad8f985
Preyas-Saxena <preyas.saxena@gmail.com>
2020-05-29 16:31:34 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,14 @@
 import numpy as np
 from keras import optimizers
 import skimage.io as io
-
 from Custom_Model import unet as seg # Importing the Unet model defined earlier
 
+#Training the model with binary cross entropy (or dice loss) for 5 epochs:
 adam = optimizers.Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 seg.compile(optimizer=adam, loss='binary_crossentropy', metrics=['binary_crossentropy'])
-seg.fit(train_x, train_y, epochs=3, batch_size=16, verbose=1, validation_split=0.10)
+seg.fit(train_x, train_y, epochs=5, batch_size=16, verbose=1, validation_split=0.10)
 
-#6th Step- Visualizing the model results on selected test images
+#Visualizing the model results on selected test images:
 
 img=train_x[50]
 io.imshow(img)
